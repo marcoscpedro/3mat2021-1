9624412fc5bcf550b1c7aaef9c7b3b3bfb645e64
MarcosMelo <marcoscarvmelo@gmail.com>
2021-02-22 09:10:05 -0300

    Aula dia 22

---
+++ revisao/arrowfunction.js
@@ -0,0 +1,13 @@
+// 1) Os parametros em torno do parametro foram omitidos
+// 2) A palavra function que vinha antes do parametro foi substituida
+// pelo simbolo => depois do parametro
+// 3) as chaves foram omitidas
+// 4) A palavra-chave return foi omitida
+
+const quadrado = (x) => {
+    return x*x
+}
+
+const potencia = (a,b) => a ** b
+
+console.log(quadrado(9))
---
+++ revisao/comparacao.js
@@ -0,0 +1,17 @@
+let a = 10, b = 15, c = 10
+console.log(a > b)
+console.log(a < b)
+console.log(a === c)
+console.log(a != c)
+
+console.log("MMMSMSM")
+
+a = "MRM"
+b = "mrmeida"
+c = "MRM"
+
+console.log(a > b)
+console.log(a < b)
+console.log(a === c)
+console.log(a != c)
+
